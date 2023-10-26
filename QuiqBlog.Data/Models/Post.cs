94471891421c629d00b58ba2e7d103172cbbf418
SHIVAM <shivamyadav9735@gmail.com>
2023-10-26 19:27:51 +0530

    Finished

---
+++ QuiqBlog.Data/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuiqBlog.Data.Models {
+    public class Post {
+        public Post()
+        {
+            // Set the default value of the Approved property to true.
+            Approved = true;
+        }
+        public int Id { get; set; }
+        public ApplicationUser Creator { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public DateTime UpdatedOn { get; set; }
+        public bool Published { get; set; }
+
+        public bool Approved { get; set; }
+        public ApplicationUser Approver { get; set; }
+
+        public virtual IEnumerable<Comment> Comments { get; set; }
+    }
+}
